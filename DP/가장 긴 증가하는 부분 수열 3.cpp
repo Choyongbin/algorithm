d47a7ce0a0ce537baa24e931780d305be5e8cced
ChoYongBin <x7520@naver.com>
2021-11-09 19:58:44 +0900

    BOJ_12738
    lower_bound
    INT_MIN

---
+++ DP/가장 긴 증가하는 부분 수열 3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int main() {
+	int n, answer = 0, num;
+	vector<int> arr;
+	arr.push_back(INT_MIN);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num > arr.back()) {
+			arr.push_back(num);
+			answer++;
+		}
+		else {
+			auto it = lower_bound(arr.begin(), arr.end(), num);
+			arr[it - arr.begin()] = num; // *it = num
+		}
+	}
+	cout << answer << "\n";
+	return 0;
+}
