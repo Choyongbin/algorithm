f30c633a85d91c46c49d75909d4233fef9a9cc73
ChoYongBin <x7520@naver.com>
2021-11-10 18:09:44 +0900

    BOJ_11279
    priority_queue

---
+++ Queue/최대힙.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>	 
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n, num;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num > 0)
+			pq.push(num);
+		else {
+			if (pq.empty())
+				cout << "0\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+
+	}
+	return 0;
+}
